0f73b32dc994278590c8f01169e17491ab4c0f79
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2024-03-14 12:57:01 -0500

    Fix errors in sampler

--- pacdb/sampler.py
+++ pacdb/sampler.py
@@ -7,66 +7,7 @@ from pyspark.sql.column import Column
 from typeguard import typechecked
 from functools import wraps
 from abc import ABC, abstractmethod
-import tqdm
-
-def with_composite_key_column(df: DataFrame, columns: List[str], key_column_name: Optional[str] = None) -> Tuple[DataFrame, str]:
-    """
-    Add a column to the dataframe that is a concatenation of the values in the specified columns
-    Used for sampling proportionately
-    Args:
-        df (DataFrame): The dataset
-        columns_to_sample_by (List[str]): Columns from the dataset df that are to be sampled
-        key_column_name (str): Composite key
-
-    Returns:
-        Tuple: Dataframe with a new concatenated column and the column name
-    """
-
-    assert all(c in df.columns for c in columns), "All columns must be present in the dataframe"
-
-    if key_column_name is None:
-        key_column_name = "_".join(columns)
-
-    if key_column_name in df.columns:
-        i: int = 0
-        while f"{key_column_name}_{i}" in df.columns:
-            i += 1
-        key_column_name = f"{key_column_name}_{i}"
-    
-    return (df.withColumn(key_column_name, concat_ws("_", *[col(c) for c in columns])), key_column_name)
-
-def sample_proportionately(df: DataFrame, 
-                        columns_to_sample_by: List[str],
-                        fraction: float,
-                        **kwargs) -> DataFrame:
-    """
-    Method to sample the data using specific columns
-    Args:
-        df (DataFrame): The dataset
-        columns_to_sample_by (List[str]): Columns from the dataset df that are to be sampled
-        fraction (float)
-
-    Returns:
-        DataFrame: sampled dataframe
-    """
-    # kwargs include seed, replacement, and other options for sampleBy
-
-    # creating a composite key in case of multiple columns
-    df_keyed, key_column = with_composite_key_column(df, columns_to_sample_by) # create key column
-
-    # establish the fraction of each label in the dataset
-    key_counts: DataFrame = df_keyed.groupBy(key_column).count()
-    total_count: int = df_keyed.count()
-
-    _key_fractions: List[Row] = (key_counts.withColumn("fraction", (col("count") / total_count) * fraction)
-                                       .select(key_column, col("fraction"))
-                                       .collect())
-    key_fractions: Dict = {row[key_column]: row["fraction"] for row in _key_fractions}
-
-    print(key_fractions)
-    
-    # attempt sampling to match the given fractions
-    return df_keyed.sampleBy(key_column, fractions=key_fractions, **kwargs).drop(key_column)
+from tqdm import tqdm
 
 @dataclass
 class SamplerOptions():
@@ -119,25 +60,25 @@ class DataFrameSampler(Sampler):
         super().__init__(df)
 
 
-    def get_samples(self, df: DataFrame, user_defined_sampling_rate: Optional[float] = 0.5, query_filter: Optional[str] = None, aggr: Optional[Any] = None, trials: Optional[int] = 100) -> list:
+    def get_samples(self, query_filter: Optional[str] = None, aggr: Optional[Any] = None, trials: int = 100) -> list:
         samples = []
 
         for i in tqdm(range(trials)):
             if query_filter is not None and aggr is not None:
-                s = (df.sample()
+                s = (self.sample()
                     .filter(query_filter)  # run query on sample
                     .agg(aggr))
             elif query_filter is None:
-                s = (df.sample()
+                s = (self.sample()
                     .agg(aggr))
             elif aggr is None:
-                s = (df.sample()
+                s = (self.sample()
                     .filter(query_filter))  # run query on sample
 
             samples.append(s)  # store result of query
         
         # TODO: change based on the aggregation being performed
-        samples = [s * (1/user_defined_sampling_rate) for s in samples]  # so that counts are not halved
+        samples = [s * (1/self.options.fraction) for s in samples]  # so that counts are not halved
 
         return samples
 
@@ -150,20 +91,3 @@ class DataFrameSampler(Sampler):
             raise ValueError("No dataframe attached to this sampler")
         return self.df.sample(withReplacement=self.options.withReplacement, fraction=self.options.fraction, seed=self.options.seed)
     
-    def sampleByColumns(self, cols: List[str]) -> DataFrame:
-        """
-        Take a single sample of the dataframe, enforcing the restriction that all categories in the specified
-        columns must be evenly represented in the sample.
-
-        Example:
-        `sampleByColumns(["column1"])` where column1 is a categorical column with options ["cats", "dogs"]
-        will try to return a DataFrame with an equal number of "cats" and "dogs" in the sample.
-
-        The proportions in the output may not be exact; the behavior of this function will match PySpark's
-        sampleBy function.
-        """
-        if self.df is None:
-            raise ValueError("No dataframe attached to this sampler")
-        elif self.options.columns_to_sample_by is None:
-            raise ValueError("No columns to sample by specified, use withOption(s) to set")
-        return sample_proportionately(self.df, cols, self.options.fraction, seed=self.options.seed)
