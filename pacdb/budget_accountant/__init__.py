0e51b20a65d1226c1dea57da59c9696b3b04709c
chaitanyasuma <chaitanyasuma@gmail.com>
2024-04-09 14:55:06 -0500

    replicating unoverlapping changes from dev-chai

---
+++ pacdb/budget_accountant/__init__.py
@@ -0,0 +1,1 @@
+from .budget_accountant import *
---
+++ pacdb/budget_accountant/budget_accountant.py
@@ -0,0 +1,9 @@
+"""Budget Accountant Class"""
+class BudgetAccountant:
+
+    def __init__(self, max_mi) -> None:
+        self.privacy_budget = max_mi
+
+
+    # TODO: Implement methods to track MI consumption - 
+    # I presume this will be needed for the cases in which we consume the budget more than once
