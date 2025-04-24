ed7f8af93a321fc66e9f629f371003a1a7e3e22a
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2025-04-24 18:17:26 -0500

    Add automated query runner for step1 in pac-duckdb-step1.py

---
+++ queries/q1.sql
@@ -0,0 +1,72 @@
+-- Modified TPC-H q1 to run on 50% subsamples
+
+
+--var:SAMPLES = 1024
+--var:INDEX_COLS = ['l_returnflag', 'l_linestatus']
+--var:OUTPUT_COLS = ['sum_qty', 'sum_base_price', 'sum_disc_price', 'sum_charge', 'avg_qty', 'avg_price', 'avg_disc', 'count_order']
+
+
+--begin SAMPLE_STEP--
+DROP TABLE IF EXISTS random_samples;
+
+CREATE TABLE random_samples AS
+WITH sample_numbers AS MATERIALIZED (
+    SELECT range AS sample_id FROM range(1024//2)
+), random_values AS MATERIALIZED (
+    SELECT 
+        sample_numbers.sample_id,
+        customer.rowid AS row_id,
+        (RANDOM() > 0.5)::BOOLEAN AS random_binary
+    FROM sample_numbers
+    JOIN customer ON TRUE  -- Cross join to duplicate rows for each sample
+)
+SELECT
+    sample_id,
+    row_id,
+    random_binary
+FROM random_values
+UNION ALL
+SELECT -- select the complementary samples too
+    (1024//2) + sample_id,
+    row_id,
+    NOT random_binary  -- Inverse the random_binary to get the complementary sample
+FROM random_values
+ORDER BY sample_id, row_id;
+--end SAMPLE_STEP--
+
+
+--begin PREPARE_STEP--
+DEALLOCATE PREPARE run_query;
+
+PREPARE run_query AS 
+SELECT
+    l_returnflag,
+    l_linestatus,
+    2*sum(l_quantity) AS sum_qty,
+    2*sum(l_extendedprice) AS sum_base_price,
+    2*sum(l_extendedprice * (1 - l_discount)) AS sum_disc_price,
+    2*sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) AS sum_charge,
+    avg(l_quantity) AS avg_qty,
+    avg(l_extendedprice) AS avg_price,
+    avg(l_discount) AS avg_disc,
+    2*count(*) AS count_order
+FROM
+    lineitem
+JOIN orders ON lineitem.l_orderkey = orders.o_orderkey
+JOIN customer ON orders.o_custkey = customer.c_custkey
+JOIN random_samples AS rs
+    ON rs.row_id = customer.rowid
+WHERE
+    l_shipdate <= CAST('1998-09-02' AS date)
+    AND rs.random_binary = TRUE
+    AND rs.sample_id = $sample
+GROUP BY
+    l_returnflag,
+    l_linestatus
+ORDER BY
+    l_returnflag,
+    l_linestatus;
+--end PREPARE_STEP--
+
+
+EXECUTE run_query(sample := 0);
