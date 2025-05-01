8ba8bce45a8a1ba2796c7a99373e951905f6b189
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2025-05-01 04:29:00 -0500

    Add Q14

---
+++ queries/q14-customer.sql
@@ -0,0 +1,63 @@
+--var:SAMPLES = 1024
+--var:INDEX_COLS = []
+--var:OUTPUT_COLS = ['promo_revenue']
+
+--begin SAMPLE_STEP--
+DROP TABLE IF EXISTS random_samples;
+
+CREATE TABLE random_samples AS
+WITH sample_numbers AS MATERIALIZED (
+    SELECT range AS sample_id FROM range(1024 // 2)
+), random_values AS MATERIALIZED (
+    SELECT 
+        sample_numbers.sample_id,
+        customer.rowid AS row_id,
+        (RANDOM() > 0.5)::BOOLEAN AS random_binary
+    FROM sample_numbers
+    JOIN customer ON TRUE  -- Cross join to duplicate rows for each sample
+)
+SELECT
+    sample_id,
+    row_id,
+    random_binary
+FROM random_values
+UNION ALL
+SELECT -- select the complementary samples too
+    (1024 // 2) + sample_id,
+    row_id,
+    NOT random_binary  -- Inverse the random_binary to get the complementary sample
+FROM random_values
+ORDER BY sample_id, row_id;
+--end SAMPLE_STEP--
+
+
+--begin PREPARE_STEP--
+DEALLOCATE PREPARE run_query;
+
+PREPARE run_query AS 
+SELECT
+    100.00 * sum(
+        CASE WHEN p_type LIKE 'PROMO%' THEN
+            l_extendedprice * (1 - l_discount)
+        ELSE
+            0
+        END) / sum(l_extendedprice * (1 - l_discount)) AS promo_revenue
+FROM
+    lineitem,
+    part,
+    orders,
+    customer,
+    random_samples AS rs
+WHERE
+    rs.row_id = customer.rowid
+    AND rs.random_binary = TRUE
+    AND rs.sample_id = $sample
+    AND o_custkey = c_custkey
+    AND o_orderkey = l_orderkey
+    AND l_partkey = p_partkey
+    AND l_shipdate >= date '1995-09-01'
+    AND l_shipdate < CAST('1995-10-01' AS date);
+--end PREPARE_STEP--
+
+
+EXECUTE run_query(sample := 0);
