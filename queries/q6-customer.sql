2da84e778c206bd3521bcd54f478868a7c25c3f1
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2025-05-01 03:22:35 -0500

    Add ability to handle outputs with no group-by columns, Q6

---
+++ queries/q6-customer.sql
@@ -0,0 +1,58 @@
+--var:SAMPLES = 1024
+--var:INDEX_COLS = []
+--var:OUTPUT_COLS = ['revenue']
+
+--begin SAMPLE_STEP--
+DROP TABLE IF EXISTS random_samples;
+
+CREATE TABLE random_samples AS
+WITH sample_numbers AS MATERIALIZED (
+    SELECT range AS sample_id FROM range(1024 // 2)
+), random_values AS MATERIALIZED (
+    SELECT 
+        sample_numbers.sample_id,
+        customer.rowid AS row_id,
+        (RANDOM() > 0.5)::BOOLEAN AS random_binary
+    FROM sample_numbers
+    JOIN customer ON TRUE  -- Cross join to duplicate rows for each sample
+)
+SELECT
+    sample_id,
+    row_id,
+    random_binary
+FROM random_values
+UNION ALL
+SELECT -- select the complementary samples too
+    (1024 // 2) + sample_id,
+    row_id,
+    NOT random_binary  -- Inverse the random_binary to get the complementary sample
+FROM random_values
+ORDER BY sample_id, row_id;
+--end SAMPLE_STEP--
+
+
+--begin PREPARE_STEP--
+DEALLOCATE PREPARE run_query;
+
+PREPARE run_query AS 
+SELECT
+    sum(l_extendedprice * l_discount) AS revenue
+FROM
+    lineitem
+JOIN orders ON lineitem.l_orderkey = orders.o_orderkey
+JOIN (
+    SELECT * FROM customer
+    JOIN random_samples AS rs ON rs.row_id = customer.rowid
+    AND rs.random_binary = TRUE
+    AND rs.sample_id = $sample
+) AS customer ON orders.o_custkey = customer.c_custkey
+WHERE
+    l_shipdate >= CAST('1994-01-01' AS date)
+    AND l_shipdate < CAST('1995-01-01' AS date)
+    AND l_discount BETWEEN 0.05
+    AND 0.07
+    AND l_quantity < 24;
+--end PREPARE_STEP--
+
+
+EXECUTE run_query(sample := 0);
