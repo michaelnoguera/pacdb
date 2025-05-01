d2dc8f8fd72e4a86a501a742e394511e1d9d56f9
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2025-05-01 03:55:56 -0500

    Add Q9

---
+++ queries/q9-customer.sql
@@ -0,0 +1,77 @@
+--var:SAMPLES = 1024
+--var:INDEX_COLS = ['nation', 'o_year']
+--var:OUTPUT_COLS = ['sum_profit']
+
+--begin SAMPLE_STEP--
+DROP TABLE IF EXISTS random_samples;
+
+CREATE TABLE random_samples AS
+WITH sample_numbers AS MATERIALIZED (
+    SELECT range AS sample_id FROM range(1024 // 2)
+), random_values AS MATERIALIZED (
+    SELECT 
+        sample_numbers.sample_id,
+        customer.rowid AS row_id,
+        (RANDOM() > 0.5)::BOOLEAN AS random_binary
+    FROM sample_numbers
+    JOIN customer ON TRUE  -- Cross join to duplicate rows for each sample
+)
+SELECT
+    sample_id,
+    row_id,
+    random_binary
+FROM random_values
+UNION ALL
+SELECT -- select the complementary samples too
+    (1024 // 2) + sample_id,
+    row_id,
+    NOT random_binary  -- Inverse the random_binary to get the complementary sample
+FROM random_values
+ORDER BY sample_id, row_id;
+--end SAMPLE_STEP--
+
+
+--begin PREPARE_STEP--
+DEALLOCATE PREPARE run_query;
+
+PREPARE run_query AS 
+SELECT
+    nation,
+    o_year,
+    sum(amount) AS sum_profit
+FROM (
+    SELECT
+        n_name AS nation,
+        extract(year FROM o_orderdate) AS o_year,
+        l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity AS amount
+    FROM
+        part,
+        supplier,
+        lineitem,
+        partsupp,
+        orders,
+        nation,
+        customer,
+        random_samples AS rs
+    WHERE
+        rs.row_id = customer.rowid
+        AND rs.random_binary = TRUE
+        AND rs.sample_id = $sample
+        AND o_custkey = c_custkey
+        AND s_suppkey = l_suppkey
+        AND ps_suppkey = l_suppkey
+        AND ps_partkey = l_partkey
+        AND p_partkey = l_partkey
+        AND o_orderkey = l_orderkey
+        AND s_nationkey = n_nationkey
+        AND p_name LIKE '%green%') AS profit
+GROUP BY
+    nation,
+    o_year
+ORDER BY
+    nation,
+    o_year DESC;
+--end PREPARE_STEP--
+
+
+EXECUTE run_query(sample := 0);
