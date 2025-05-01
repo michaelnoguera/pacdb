baa47036fb2be4db7d57911b3d8cae328fd4d43c
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2025-05-01 04:16:02 -0500

    Add Q13

---
+++ queries/q13-customer.sql
@@ -0,0 +1,63 @@
+--var:SAMPLES = 1024
+--var:INDEX_COLS = ['c_count']
+--var:OUTPUT_COLS = ['custdist']
+
+--begin SAMPLE_STEP--
+DROP TABLE IF EXISTS random_samples;
+
+CREATE TABLE random_samples AS
+WITH sample_numbers AS MATERIALIZED (
+    SELECT range AS sample_id FROM range(1024 // 2)
+), random_values AS MATERIALIZED (
+    SELECT 
+        sample_numbers.sample_id,
+        customer.rowid AS row_id,
+        (RANDOM() > 0.5)::BOOLEAN AS random_binary
+    FROM sample_numbers
+    JOIN customer ON TRUE  -- Cross join to duplicate rows for each sample
+)
+SELECT
+    sample_id,
+    row_id,
+    random_binary
+FROM random_values
+UNION ALL
+SELECT -- select the complementary samples too
+    (1024 // 2) + sample_id,
+    row_id,
+    NOT random_binary  -- Inverse the random_binary to get the complementary sample
+FROM random_values
+ORDER BY sample_id, row_id;
+--end SAMPLE_STEP--
+
+
+--begin PREPARE_STEP--
+DEALLOCATE PREPARE run_query;
+
+PREPARE run_query AS 
+SELECT
+    c_count,
+    count(*) AS custdist
+FROM (
+    SELECT
+        c_custkey,
+        count(o_orderkey)
+    FROM
+        (SELECT * FROM customer
+        JOIN random_samples AS rs ON rs.row_id = customer.rowid
+        AND rs.random_binary = TRUE
+        AND rs.sample_id = $sample) AS customer
+    LEFT OUTER JOIN orders ON c_custkey = o_custkey
+    AND o_comment NOT LIKE '%special%requests%'
+GROUP BY
+    c_custkey) AS c_orders (c_custkey,
+        c_count)
+GROUP BY
+    c_count
+ORDER BY
+    custdist DESC,
+    c_count DESC;
+--end PREPARE_STEP--
+
+
+EXECUTE run_query(sample := 0);
