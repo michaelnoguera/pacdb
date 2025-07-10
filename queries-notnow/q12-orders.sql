4d7a88fb93db55c9c62c293e7a4d43b869a37692
Mayuri Sridhar <mayuri@Mayuris-MacBook-Air-2.local>
2025-07-10 10:55:12 -0400

    move notebooks to separate directory and add order subsampling for q4, q12, q14

---
+++ queries-notnow/q12-orders.sql
@@ -0,0 +1,71 @@
+--var:SAMPLES = 128
+--var:INDEX_COLS = ['l_shipmode']
+--var:OUTPUT_COLS = ['high_line_count', 'low_line_count']
+
+--begin SAMPLE_STEP--
+DROP TABLE IF EXISTS random_samples;
+
+CREATE TABLE random_samples AS
+WITH sample_numbers AS MATERIALIZED (
+    SELECT range AS sample_id FROM range(128)
+), random_values AS MATERIALIZED (
+    SELECT 
+        sample_numbers.sample_id,
+        orders.rowid AS row_id,
+        (RANDOM() > 0.5)::BOOLEAN AS random_binary
+    FROM sample_numbers
+    JOIN orders ON TRUE  -- Cross join to duplicate rows for each sample
+)
+SELECT
+    sample_id,
+    row_id,
+    random_binary
+FROM random_values
+ORDER BY sample_id, row_id;
+--end SAMPLE_STEP--
+
+
+--begin PREPARE_STEP--
+DEALLOCATE PREPARE run_query;
+
+PREPARE run_query AS 
+SELECT
+    l_shipmode,
+    sum(
+        CASE WHEN o_orderpriority = '1-URGENT'
+            OR o_orderpriority = '2-HIGH' THEN
+            1
+        ELSE
+            0
+        END) AS high_line_count,
+    sum(
+        CASE WHEN o_orderpriority <> '1-URGENT'
+            AND o_orderpriority <> '2-HIGH' THEN
+            1
+        ELSE
+            0
+        END) AS low_line_count
+FROM
+    orders,
+    lineitem,
+    customer,
+    random_samples AS rs
+WHERE
+    rs.row_id = orders.rowid
+    AND rs.random_binary = TRUE
+    AND rs.sample_id = $sample
+    AND o_custkey = c_custkey
+    AND o_orderkey = l_orderkey
+    AND l_shipmode IN ('MAIL', 'SHIP')
+    AND l_commitdate < l_receiptdate
+    AND l_shipdate < l_commitdate
+    AND l_receiptdate >= CAST('1994-01-01' AS date)
+    AND l_receiptdate < CAST('1995-01-01' AS date)
+GROUP BY
+    l_shipmode
+ORDER BY
+    l_shipmode;
+--end PREPARE_STEP--
+
+
+EXECUTE run_query(sample := 0);
