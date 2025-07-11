d751fc2020056e82eea6715f439ebf593203a807
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2025-07-11 04:12:45 -0500

    Add Q5 optimization Merge branch 'michael-timing' into main

---
+++ queries-notnow/q5-customer-no-prejoin.sql
@@ -0,0 +1,62 @@
+--var:SAMPLES = 1024
+--var:INDEX_COLS = ['n_name']
+--var:OUTPUT_COLS = ['revenue']
+
+--begin SAMPLE_STEP--
+DROP TABLE IF EXISTS random_samples;
+
+CREATE TABLE random_samples AS
+WITH sample_numbers AS MATERIALIZED (
+    SELECT range AS sample_id FROM range(1024)
+), random_values AS MATERIALIZED (
+    SELECT
+        sample_numbers.sample_id,
+        customer.rowid AS row_id,
+        (RANDOM() > 0.5) AS random_binary
+    FROM sample_numbers
+    JOIN customer ON TRUE  -- Cross join to duplicate rows for each sample
+)
+SELECT
+    sample_id,
+    row_id
+FROM random_values
+WHERE random_binary = TRUE
+ORDER BY sample_id, row_id;
+--end SAMPLE_STEP--
+
+
+--begin PREPARE_STEP--
+DEALLOCATE PREPARE run_query;
+
+PREPARE run_query AS 
+SELECT
+    n_name,
+    sum(l_extendedprice * (1 - l_discount)) AS revenue
+FROM
+    (SELECT * FROM customer
+              JOIN random_samples AS rs
+              ON rs.row_id = customer.rowid
+              AND rs.sample_id = $sample::INT) as customer,
+    orders,
+    lineitem,
+    supplier,
+    nation,
+    region
+WHERE
+    c_custkey = o_custkey
+    AND l_orderkey = o_orderkey
+    AND l_suppkey = s_suppkey
+    AND c_nationkey = s_nationkey
+    AND s_nationkey = n_nationkey
+    AND n_regionkey = r_regionkey
+    AND r_name = 'ASIA'
+    AND o_orderdate >= CAST('1994-01-01' AS date)
+    AND o_orderdate < CAST('1995-01-01' AS date)
+GROUP BY
+    n_name
+ORDER BY
+    revenue DESC;
+--end PREPARE_STEP--
+
+
+EXECUTE run_query(sample := 0);
